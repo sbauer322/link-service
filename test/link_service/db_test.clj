dfb39a7ae46e71d4dbc38c256c56a7974f082b0f
Scott Bauer <sbauer322@gmail.com>
2015-09-23 23:53:34 -0400

    Added some basic testing and made note of how to run the tests in the README.

---
+++ test/link_service/db_test.clj
@@ -0,0 +1,3 @@
+(ns link-service.db-test
+  (:require [clojure.test :refer :all]
+            [link-service.db :refer :all]))
--- test/link_service/handler_test.clj
+++ test/link_service/handler_test.clj
@@ -1,13 +1,20 @@
 (ns link-service.handler-test
   (:require [clojure.test :refer :all]
             [ring.mock.request :as mock]
-            [link-service.handler :refer :all]))
+            [link-service.handler :refer :all]
+            [link-service.db :as db]))
 
 (deftest test-app
+  (testing "random"
+    (with-redefs [db/random-link (fn [] "foo")]
+      (let [response (app (mock/request :get "/random"))]
+        (is (= (:status response) 200))
+        (is (= (:body response) "\"foo\"")))))
+
   (testing "main route"
     (let [response (app (mock/request :get "/"))]
-      (is (= (:status response) 200))
-      (is (= (:body response) "Hello World"))))
+      (is (= (:status response) 404))
+      (is (= (:body response) "Not Found"))))
 
   (testing "not-found route"
     (let [response (app (mock/request :get "/invalid"))]
