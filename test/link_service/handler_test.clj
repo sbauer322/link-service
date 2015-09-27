89a9348e7d8909ae10ffd639af3bd4f9f34df4e2
Scott Bauer <sbauer322@gmail.com>
2015-09-26 23:00:37 -0400

    Change response messages to JSON.

--- test/link_service/handler_test.clj
+++ test/link_service/handler_test.clj
@@ -11,6 +11,34 @@
         (is (= (:status response) 200))
         (is (= (:body response) "{\"link\":\"foo\"}")))))
 
+  (testing "add link - bad token"
+    (with-redefs [db/add-link (fn [_] {:link "foo"})]
+      (let [response (app (mock/body
+                           (mock/content-type
+                            (mock/request :post "/add")
+                            "application/json")
+                           "{\"link\": \"https://agithusb.com/ring-csdflojure/ring\", \"token\": \"nonuser\"}"))]
+        (is (= (:status response) 200))
+        (is (= (:body response) "{\"success?\":false,\"message\":\"Invalid token!\"}")))))
+
+  (testing "add link - good token"
+    (with-redefs [db/add-link (fn [_] {:link "foo"})]
+      (let [response (app (mock/body
+                           (mock/content-type
+                            (mock/request :post "/add")
+                            "application/json")
+                           "{\"link\": \"https://github.com/\", \"token\": \"user\"}"))]
+        (is (= (:status response) 200))
+        (is (= (:body response) "{\"success?\":true,\"message\":\"Added link: https://github.com/\"}")))))
+
+  ; (testing "add good token"
+  ;   )
+
+  ;  {
+  ;  "success?": true,
+  ;   "message": "Added link: https://agithusb.com/ring-csdflojure/ring"
+  ;   }
+
   (testing "default route"
     (let [response (app (mock/request :get "/"))]
       (is (= (:status response) 404))
